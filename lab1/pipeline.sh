5db381028a31910c29d4f905c3dfc4e6d66ccfb8
Gorshenin Alexander <103529423+Roaoch@users.noreply.github.com>
2026-05-30 01:01:59 +0500

    lab2

--- lab1/pipeline.sh
+++ lab1/pipeline.sh
@@ -1,6 +1,7 @@
+#!/bin/bash
+
 python3 -m venv .venv
-.venv/bin/pip install "torch>=2.10.0" --index-url https://download.pytorch.org/whl/cpu
-.venv/bin/pip install "datasets>=4.6.1" "gensim>=4.4.0" "scikit-learn>=1.8.0" "triton>=3.6.0"
+.venv/bin/pip install -r requirements.txt
 
 .venv/bin/python "data_creation.py" -q
 .venv/bin/python "data_preprocessing.py" -q
